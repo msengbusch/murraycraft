c6a0a547ffb2a8d36b4b0c5ba834ec76a1dd5d2e
msengbusch <marek.sengbusch@flg-online.de>
2021-10-23 09:52:05 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    kotlin("jvm") version "1.5.31" apply false
+}
+
+allprojects {
+    group = "io.github.msengbusch"
+    version = "0.1.0-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+
+    }
+}
